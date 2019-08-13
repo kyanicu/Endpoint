501ddfa87c219ed72c7559d0041e52b357650c5e
jmccarty19 <47871847+jmccarty19@users.noreply.github.com>
2019-08-12 23:11:05 -0400

    Created Body Swap Prototype

---
+++ Space Core/BodySwappingPrototype/Assets/Resources/Scripts/QTEButton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QTEButton : MonoBehaviour
+{
+    public enum KeyNames
+    {
+        up,
+        down,
+        left,
+        right
+    };
+    public KeyNames keyName { get; private set; }
+    private int[] arrowRotation =
+    {
+        180,
+        0,
+        270,
+        90
+    };
+    public bool active;
+    public GameObject arrowImage;
+    private Image button;
+
+    public void setColor(Color color)
+    {
+        button.color = color;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range(0, 3);
+        keyName = (KeyNames)rand;
+        Vector3 rotValue = new Vector3(0, 0, arrowRotation[rand]);
+        arrowImage.transform.Rotate(rotValue);
+        active = false;
+        button = GetComponent<Image>();
+        setColor(Color.gray);
+    }
+}
---
+++ Space Core/BodySwappingPrototype/Assets/Resources/Scripts/QTEManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QTEManager : MonoBehaviour
+{
+    private KeyCode[] keys =
+{
+        KeyCode.UpArrow,
+        KeyCode.DownArrow,
+        KeyCode.LeftArrow,
+        KeyCode.RightArrow
+    };
+    private GameObject qteButton;
+    private Stack<GameObject> buttonStack = new Stack<GameObject>();
+    public Transform[] ButtonLoc;
+    private QTEButton activeButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        qteButton = Resources.Load<GameObject>("Prefabs/QTE Button");
+        StackPush(3);
+    }
+    
+    public void StackPush(int numItems)
+    {
+        for (int i = 0; i < numItems; i++)
+        {
+            GameObject button = Instantiate(qteButton);
+            if (i < 3)
+            {
+                button.transform.position = ButtonLoc[i].position;
+            }
+            buttonStack.Push(button);
+        }
+        ActivateButton();
+    }
+
+    /// <summary>
+    /// Activates first button in stack and changes color to active color
+    /// </summary>
+    private void ActivateButton()
+    {
+        activeButton = buttonStack.Peek().GetComponent<QTEButton>();
+        activeButton.active = true;
+        activeButton.setColor(Color.white);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activeButton.active)
+        {
+            if (Input.GetKeyDown(keys[(int)activeButton.keyName]))
+            {
+                activeButton.setColor(Color.green);
+                buttonStack.Pop();
+                ActivateButton();
+            }
+            else
+            {
+                StartCoroutine(ClearStack());
+            }
+        }
+    }
+
+    private IEnumerator ClearStack()
+    {
+        activeButton.setColor(Color.red);
+        yield return new WaitForSeconds(2);
+        for(int i = buttonStack.Count; i >= 0; i--)
+        {
+            GameObject button = buttonStack.Peek();
+            if (button != null)
+            {
+                Destroy(button);
+            }
+            buttonStack.Pop();
+        }
+        yield return null;
+    }
+}
