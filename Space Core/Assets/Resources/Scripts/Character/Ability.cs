d9881af299e2fc34542e38e180ac578f04cb0f35
Palheta Leitao <gjp45@drexel.edu>
2019-11-04 14:14:28 -0500

    Implemented Ability system

---
+++ Space Core/Assets/Resources/Scripts/Character/Ability.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Ability : MonoBehaviour
+{
+    public enum AbilityType { PASSIVE, ACTIVE };
+
+    public AbilityType type { get; protected set; }
+
+    public abstract void Activate();
+
+}
---
+++ Space Core/Assets/Resources/Scripts/Character/PassiveAbility.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PassiveAbility : Ability
+{
+
+    void Awake()
+    {
+        type = Ability.AbilityType.PASSIVE;
+    }
+
+    protected abstract bool CheckActivation();
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CheckActivation())
+            Activate();
+    }
+}
