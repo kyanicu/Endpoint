e71ba5bbbdbbc86c44720a61482b8fd043516e37
jmccarty19 <47871847+jmccarty19@users.noreply.github.com>
2019-11-11 15:40:53 -0500

    Added Hazard Scripts

---
+++ Space Core/Assets/Resources/Scripts/Environment/Lava.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Player.instance.TakeDamage(Player.instance.MaxHealth / 4);
+        //Return player to last location they were at
+    }
+}
---
+++ Space Core/Assets/Resources/Scripts/Environment/ShockFloor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockFloor : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            Player.instance.TakeDamage(15);
+        }
+    }
+}
