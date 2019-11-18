37cd3ca1e6533f5b639de7ad971b75574a3d9bd0
KyleWatsonDev <45539809+KyleWatsonDev@users.noreply.github.com>
2019-11-18 16:49:48 -0500

    Level is now set up

---
+++ Space Core/Assets/Resources/Scripts/Environment/Terrain.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terrain : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Contains("Bullet"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
