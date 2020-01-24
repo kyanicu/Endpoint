1fef37d30ed09c3b962385702c2fc2318ae8f54e
kyanicu <53977761+kyanicu@users.noreply.github.com>
2020-01-24 02:04:38 -0500

    Moving platform (#110)
    * Set up abstract movement classes for both enemy and player
    
    * abstracted movement
    
    * added comments
    
    * Enemies now use Movement class
    
    * Enemies now move with Character.Movement. Still needs some fine tuning
    
    * Quick update
    
    * started working on better collision between characters
    
    * This is Adam's fault.
    
    *  collision with enemies and weapons feels more natural, giving a push rather than feeling like a static wall
    
    * Working on corner bug fix
    
    * a lot of new assets and a new scene with them textured and placed so vl can test things. (#63)
    
    * Character is no longer sticky
    
    * Attempting step movement
    
    * Changed knockback to feel way better
    
    * implemented faster breaking when running in opposite direction
    
    * Started making moving platforms
    
    * Minor change
    
    * Moving platforms without player sticking on them works
    
    * Moving platforms work in every direction besides any bit up
    
    * Minor refactor for how character is moved along platform
    
    * Downward moving platforms work now
    
    * working on diagonals
    
    * Moving platforms work in all directions
    
    Co-authored-by: KyleWatsonDev <45539809+KyleWatsonDev@users.noreply.github.com>
    Co-authored-by: TamirYohan <56728660+TamirYohan@users.noreply.github.com>

--- Space Core/Assets/Resources/Scripts/Environment/MovingPlatform.cs
+++ Space Core/Assets/Resources/Scripts/Environment/MovingPlatform.cs
@@ -10,6 +10,8 @@ public class MovingPlatform : MonoBehaviour
     public ObjectMover mover;
     public Collider2D col;
 
+    List<Character> groundedCharacters = new List<Character>();
+
     private void Reset()
     {
         //Const Values
@@ -41,7 +43,76 @@ public class MovingPlatform : MonoBehaviour
     {
         
     }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player" || other.tag == "Enemy")
+        {
+            Character character = other.GetComponent<Character>();
+            int contactCount;
+            ContactData[] contacts = character.movement.UpdateState(-direction, out contactCount);
+            if (character.movement.charCont.isGrounded) 
+            {
+                for (int i = 0; i < contactCount; i++)
+                {
+                    if (contacts[i].collider == null)
+                        continue;
+                    else if (contacts[i].collider.gameObject == gameObject && character.movement.charCont.CheckGroundableSlope(contacts[i].normal))
+                    {
+                        groundedCharacters.Add(character);
+                    }
+                }
+            }
+        }
+    }
+/*
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        
+        if (direction.y < 0 && (other.tag == "Player" || other.tag == "Enemy"))
+        {
 
+            Character character = other.GetComponent<Character>();
+            int contactCount;
+            ContactData[] contacts = character.movement.UpdateState(direction, out contactCount);
+            if (character.movement.charCont.isGrounded) 
+            {
+                for (int i = 0; i < contactCount; i++)
+                {
+                    if (contacts[i].collider == null)
+                        continue;
+                    else if (contacts[i].collider.gameObject == gameObject && character.movement.charCont.CheckGroundableSlope(contacts[i].normal))
+                    {
+                        groundedCharacters.Add(character);
+                    }
+                }
+            }
+            else 
+            {
+                bool prevQueriesHitTriggers = Physics2D.queriesHitTriggers;
+                Physics2D.queriesHitTriggers = true;
+
+                int maxSize = 10;
+                RaycastHit2D[] hits = new RaycastHit2D[maxSize];
+                int numHits;
+                if ((numHits = character.movement.charCont.capCol.Cast(direction, hits, Physics2D.defaultContactOffset + (speed * Time.fixedDeltaTime))) > 0)
+                {
+                    if(numHits > maxSize)
+                        numHits = maxSize;
+
+                    for (int i = 0; i < numHits; i++)
+                    {
+                        if (hits[i].collider.gameObject == gameObject && character.movement.charCont.CheckGroundableSlope(hits[i].normal))
+                        {
+                            groundedCharacters.Add(character);
+                        }
+                    }
+                }
+                Physics2D.queriesHitTriggers = prevQueriesHitTriggers;
+            }
+        }
+        
+    }
+*/
     public float speed = 5f;
     public float change = 1f;
     public Vector2 direction = Vector2.right;
@@ -57,5 +128,45 @@ public class MovingPlatform : MonoBehaviour
             timer = 0f;
         }
         mover.Move(speed * direction * Time.fixedDeltaTime);
+        
+        if(direction.y < 0)
+        {
+
+            bool prevQueriesHitTriggers = Physics2D.queriesHitTriggers;
+            Physics2D.queriesHitTriggers = true;
+
+            int maxSize = 10;
+            RaycastHit2D[] hits = new RaycastHit2D[maxSize];
+            int numHits;
+            if ((numHits = col.Cast(-direction, hits, Physics2D.defaultContactOffset + (speed * Time.fixedDeltaTime))) > 0)
+            {
+                Debug.Log("hit");
+                if(numHits > maxSize)
+                    numHits = maxSize;
+
+                for (int i = 0; i < numHits; i++)
+                {
+
+                    if (hits[i].collider.gameObject.tag == "Player" || hits[i].collider.gameObject.tag == "Enemy")
+                    {
+                        
+                        Character character = hits[i].collider.gameObject.GetComponent<Character>();
+                        int contactCount;
+                        if (character.movement.charCont.isGrounded && character.movement.charCont.CheckGroundableSlope(-hits[i].normal));
+                        {
+                            if (!groundedCharacters.Contains(character))
+                                groundedCharacters.Add(character);
+                        }
+                    }
+                }
+            }
+            Physics2D.queriesHitTriggers = prevQueriesHitTriggers;
+        }
+
+        foreach(Character character in groundedCharacters){
+            int moveCount;
+            MoveData[] moveData = character.movement.charCont.Move(speed * direction * Time.fixedDeltaTime, out moveCount, true);
+        }
+        groundedCharacters.Clear();
     }
 }
